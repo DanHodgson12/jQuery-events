a2de5f52158bf50010ca1f36d304b5ce34652669
DanHodgson12 <>
2023-08-05 11:22:27 +0000

    added css animations to certain elements using jQuery

--- script.js
+++ script.js
@@ -17,4 +17,30 @@ $(document).ready(function () {
         $(".stream3").removeClass('highlight_stream');
         $(".stream3").addClass('highlight_stream');
     });
+
+    $("p").on("click", function () {
+        $("p").css("color", "red");
+    });
+
+    $("h2").on("mouseover", function () {
+        $("h2").css("background-color", "lightblue");
+    });
+    $("h2").on("mouseout", function () {
+        $("h2").css("background-color", "#fff");
+    });
+
+    $(".card").on("mouseover", function () {
+        $("h2").css("font-size", "1.5em");
+    });
+    $(".card").on("mouseout", function () {
+        $("h2").css("font-size", "1em");
+    });
+
+    $(".bottom_button").on("mouseenter", function () {
+        $("body").css("background-color", "black");
+    });
+    $(".bottom_button").on("mouseleave", function () {
+        $("body").css("background-color", "#eee");
+    });
+
 }); 
